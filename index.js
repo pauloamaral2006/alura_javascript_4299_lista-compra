f90ce2504e7ec20d06822a2baaf54bd55536489c
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2025-03-25 10:43:33 -0300

    aula-02

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const inputItem = document.getElementById("input-item")
+let contador = 0;
 
-console.log(inputItem.value);
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+
+  if (inputItem.value === "") {
+    alert("Por favor, inisira um item!");
+    return;
+  }
+
+  const itemDaLista = document.createElement("li");
+  const containerItemDaLista = document.createElement("div");
+  containerItemDaLista.classList.add("lista-item-container");
+  const inputCheckbox = document.createElement("input");
+  inputCheckbox.type = "checkbox";
+  inputCheckbox.id = "checkbox-" + contador++;
+  const nomeItem = document.createElement("p");
+  nomeItem.innerText = inputItem.value;
+
+  containerItemDaLista.appendChild(inputCheckbox);
+  containerItemDaLista.appendChild(nomeItem);
+
+  itemDaLista.appendChild(containerItemDaLista);
+  listaDeCompras.appendChild(itemDaLista);
+});
